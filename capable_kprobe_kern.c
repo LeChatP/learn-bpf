21c6c57692479d1e4dee1ff3f9859ea1183f6738
LeChatP <lechatp@outlook.fr>
2019-05-07 17:14:18 +0200

    compiling and don't crash

--- capable_kprobe_kern.c
+++ capable_kprobe_kern.c
@@ -4,21 +4,29 @@
 #include <uapi/linux/bpf.h>
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
+#include <linux/capability.h>
 #include "bpf_helpers.h"
+
+struct bpf_map_def SEC("maps") capabilities_map = {
+	.type = 		BPF_MAP_TYPE_HASH,
+	.key_size 		= sizeof(u32),
+	.value_size		= sizeof(int),
+	.max_entries	= PID_MAX_DEFAULT,
+	.map_flags   	= 0
+};
 
 SEC("kprobe/capable")
 int bpf_prog1(struct pt_regs *ctx)
 {
+	u32 *val, cap_start = 0;
     u32 pid = bpf_get_current_pid_tgid() >> 32;
-	u32 uid = bpf_get_current_uid_gid();
 	int cap = (int)PT_REGS_PARM1(ctx); // param 1 is capability
-	unsigned long unknown1 = PT_REGS_PARM2(ctx); //???
-	unsigned long unknown2 = PT_REGS_PARM3(ctx); //???
+	//u32 uid = bpf_get_current_uid_gid();
+	//unsigned long unknown1 = PT_REGS_PARM2(ctx); //???
+	//unsigned long unknown2 = PT_REGS_PARM3(ctx); //???
 	
-	char data_format[] = "pid: %lu\tuid: %lu\tcap: %d\n";
-
-	bpf_trace_printk(data_format, sizeof(data_format), pid,uid,cap);
-
+	//val = bpf_map_lookup_elem(&capabilities_map, &pid);
+	bpf_map_update_elem(&capabilities_map, &pid, &cap_start, BPF_ANY);
 	return 0;
 }
 
--- capable_kprobe_user.c
+++ capable_kprobe_user.c
@@ -8,14 +8,24 @@
 int main(int argc, char **argv)
 {
 	char filename[256];
-
 	snprintf(filename, sizeof(filename), "%s_kern.o", argv[0]);
-
 	if (load_bpf_file(filename)) {
+		printf("%s", bpf_log_buf);
 		return 1;
 	}
-
-	read_trace_pipe();
+	
+	int key, value;
+	int res, prev_key = -1;
+	sleep(1);
+	while(bpf_map_get_next_key(map_fd[0], &prev_key, &key) == 0){
+		res = bpf_map_lookup_elem(map_fd[0], &key, &value);
+		if(res < 0 ){
+			printf("No value??\n");
+        	continue;
+		}
+		printf("got pid : %d\tcaps : %d\n",key,value);
+		prev_key=key;
+	}
 
 	return 0;
 }
